6b4ec204bccd0c19b1a06b16d5516c5944b1a5df
cogumbreiro <cogumbreiro@995af941-4be3-0310-965d-cb6edba6dabf>
2004-12-27 09:40:33 +0000

    Added totem playlist parser python bindings.

---
+++ totem-pl-parser-python/playlist_parser/pl-parser-module.c
@@ -0,0 +1,43 @@
+/* 
+   Copyright (C) 2004 Tiago Cogumbreiro <cogumbreiro@users.sf.net>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the Gnome Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Tiago Cogumbreiro <cogumbreiro@users.sf.net>
+ */
+
+#include <pygobject.h>
+#include <Python.h>
+void playlist_parser_register_classes (PyObject *d);
+
+extern PyMethodDef playlist_parser_functions[];
+
+DL_EXPORT(void)
+initplaylist_parser (void)
+{
+	PyObject *m, *d;
+	
+	init_pygobject ();
+	
+	m = Py_InitModule ("playlist_parser", playlist_parser_functions);
+	d = PyModule_GetDict (m);
+
+	playlist_parser_register_classes (d);
+	if (PyErr_Occurred ()) {
+		//PyFatalError ("can't initialize module nautilus.burn");
+	}
+	
+}
